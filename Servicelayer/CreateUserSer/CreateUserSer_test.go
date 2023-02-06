baaa8d588e00ebcb0e2e6824e2ba3984c05a04a0
Puppala Karthik <puppala.karthik@OPN-MC-012.local>
2023-02-06 12:59:04 +0530

    commited

--- Servicelayer/CreateUserSer/CreateUserSer_test.go
+++ Servicelayer/CreateUserSer/CreateUserSer_test.go
@@ -5,7 +5,7 @@ import (
 	"example/persons/db"
 	"example/persons/models"
 	"testing"
-	"github.com/gin-gonic/gin"
+	// "github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -16,12 +16,12 @@ func init() {
 
 func TestCreateUsersSer(t *testing.T) {
 person_req := models.Person{
-	"Name": "Kuradu",
-	"ID": 37,
-	"Gender": "M",
-	"BloodType": "C+",
-	"Num": 896050
+	Name: "Kuradu",
+	ID: 37,
+	Gender: "M",
+	BloodType: "C+",
+	Num: 896050,
 }
 err := CreateUserSer(person_req)
 assert.Empty(t, err)
-	}
+}
